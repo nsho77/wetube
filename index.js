3050e6f53d7c247a57764a1c32a72ace84c9eb19
unknown <nsho77@naver.com>
2020-05-16 21:56:44 +0900

    Add dependency babel, nodemon

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 
 const PORT = 4000;
 
-function handleListening(){
-    console.log("Listening on http://localhost:4000");
-}
+const handleListening = () => console.log("Listening on http://localhost:4000");
 
-function handleHome(req, res){
-    res.send('Hello from home');
-}
+const handleHome = (req, res) => res.send('Hello from home');
 
-function handleProfile(req, res){
-    res.send("You are on my profile");
-}
+const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.get("/", handleHome);
 
