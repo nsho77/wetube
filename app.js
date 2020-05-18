714611e2b4bfac02fb230a38502a5e7d363d136f
unknown <nsho77@naver.com>
2020-05-18 22:22:10 +0900

    Add router

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import {userRouter} from './router';
 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () => console.log("Listening on http://localhost:4000");
 
 const handleHome = (req, res) => res.send('Hello from home');
 
@@ -24,4 +21,6 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
